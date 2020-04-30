8f6e6b9bb9fab53629cd7fde8f62081b8ce54e15
Michael Dodge, II <mdodge@hawaii.edu>
2020-04-30 08:30:05 -1000

    Added basic skeleton of script to execute pipeline

---
+++ pipeline.sh
@@ -0,0 +1,22 @@
+#Script for Data Processing Pipeline (currently a skeleton)
+
+#Planning to be repeated - cron would likely be more elegant than trying to
+#integrate a time-based loop element
+
+
+
+### FINAL-type scripts
+
+## HADS portion
+r hads_24hr_webscape_5am_FINAL.R # may need additional arguments/setup
+
+## Aggregation portion
+r scan_daily_hourly_data_agg_FINAL.R # same as hads portion
+
+### TESTING-type scripts - currently commented until deps are sorted
+
+#r all_data_daily_merge_table_TESTING.R
+
+#r All_Islands_daily_clim_aid_kriging_TESTING.R
+
+#r nws_hrly_24hr_webscape_TEST.R
