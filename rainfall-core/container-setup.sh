34900ebb5836e90c22363400a2ff9661bfadd912
Michael Dodge, II <mdodge@hawaii.edu>
2020-05-05 14:23:55 -1000

    Added cron boilerplate, reorganized APT commands

--- rainfall-core/container-setup.sh
+++ rainfall-core/container-setup.sh
@@ -7,13 +7,25 @@ rm /etc/apt/apt.conf.d/default
 echo "//APT::Default-Release \"testing\";" > /etc/apt/apt.conf.d/default
 apt update
 
-# Next, get non-R dependencies
-apt install nano -y                     # preference
-apt install libxml2-dev -y              # RNRCS
-apt install libssl-dev -y               # openssl
-apt install curl -y                     # Rcurl
-apt install libcurl4-openssl-dev -y     # Rcurl
+# Get apt package dependencies for R packages
+apt install libxml2-dev -y              # for RNRCS
+apt install libssl-dev -y               # for openssl
+apt install curl -y                     # for Rcurl
+apt install libcurl4-openssl-dev -y     # for Rcurl
+
+# Get packages for container operations
+apt install nano -y                     # command-line text editor
+apt install cron -y                     # for automated operations
 
 ### Get necessary R-type packages
 r rainfall-prereqs.r
+
+### Set up other things
+
+# Put pipeline.sh into cron
+CRON_FILE="/var/spool/cron/crontabs/rainfall_cron"
+touch $CRON_FILE
+crontab $CRON_FILE
+echo "*/30 * * * * bash pipeline.sh >/dev/null 2>&1" > $CRON_FILE
+
 sleep infinity
