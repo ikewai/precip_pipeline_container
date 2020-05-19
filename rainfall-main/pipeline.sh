13e16404945c208696cd65b19210478eaf0bb2de
Michael Dodge, II <mdodge@hawaii.edu>
2020-05-19 11:44:14 -1000

    adjustments to pipeline.sh for getting scripts

--- rainfall-main/pipeline.sh
+++ rainfall-main/pipeline.sh
@@ -1,21 +1,15 @@
-#Script for Data Processing Pipeline (currently a skeleton)
+#Script for Data Processing Pipeline
 
-#(Proposed) Get latest version of processing scripts
+## (Proposed) Get latest version of processing scripts
+# Needs public final versions of the scripts:
+# cd <folder for scripts>
+# wget <URL of first script to grab> ... <URL of last script to grab>
 
 
+## Script Execution
 
-### FINAL-type scripts
-
-## HADS portion
+# HADS
 r hads_24hr_webscape_5am_FINAL.R # may need additional arguments/setup
 
-## Aggregation portion
+# Aggregation
 r scan_daily_hourly_data_agg_FINAL.R # same as hads portion
-
-### TESTING-type scripts - currently commented until deps are sorted
-
-#r all_data_daily_merge_table_TESTING.R
-
-#r All_Islands_daily_clim_aid_kriging_TESTING.R
-
-#r nws_hrly_24hr_webscape_TEST.R
