d9cf3ef47eb909bd4ac8559376f6eebe25b8377b
Michael Dodge, II <mdodge@hawaii.edu>
2020-05-19 11:19:02 -1000

    Removed deprecated files, pushed to dockerhub

--- rainfall-core/container-setup.sh
+++
@@ -1,9 +0,0 @@
-#POSSIBLY DEPRECATED, COMMANDS MOVED TO DOCKERFILE.
-
-# Put pipeline.sh into cron
-#CRON_FILE="/var/spool/cron/crontabs/rainfall_cron"
-#touch $CRON_FILE
-#crontab $CRON_FILE
-#echo "*/30 * * * * bash pipeline.sh >/dev/null 2>&1" > $CRON_FILE
-
-sleep infinity
