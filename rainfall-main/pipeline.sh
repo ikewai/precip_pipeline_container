fb59607ea008974762c9759d0928049d5e92e579
Michael Dodge, II <mdodge@hawaii.edu>
2020-05-20 14:38:47 -1000

    adjusted pipeline and rainfall-main for testing

--- rainfall-main/pipeline.sh
+++ rainfall-main/pipeline.sh
@@ -2,8 +2,15 @@
 
 ## Script Execution
 
+# Testing
+echo "The current time is: " `date` > /usr/src/app/testing.log
+
 # HADS
-r hads_24hr_webscape_5am_FINAL.R # may need additional arguments/setup
+#r hads_24hr_webscape_5am_FINAL.R # may need additional arguments/setup
 
 # Aggregation
-r scan_daily_hourly_data_agg_FINAL.R # same as hads portion
+#r scan_daily_hourly_data_agg_FINAL.R # same as hads portion
+
+
+exit 0
+EOF
