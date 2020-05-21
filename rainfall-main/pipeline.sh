d7789738d2050e272a83458012f3da0c9074c35c
Michael Dodge, II <mdodge@hawaii.edu>
2020-05-20 17:14:26 -1000

    Removed internal cron, set up first directories

--- rainfall-main/pipeline.sh
+++ rainfall-main/pipeline.sh
@@ -6,10 +6,10 @@
 echo "The current time is: " `date` > /usr/src/app/testing.log
 
 # HADS
-#r hads_24hr_webscape_5am_FINAL.R # may need additional arguments/setup
+#r scripts/hads_final.r
 
 # Aggregation
-#r scan_daily_hourly_data_agg_FINAL.R # same as hads portion
+#r scripts/scan_final.r
 
 
 exit 0
