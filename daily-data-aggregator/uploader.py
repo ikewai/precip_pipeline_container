494931b67c46f0d0e47b06a3d52ccabe6658c642
Michael Dodge <mdodge@hawaii.edu>
2021-03-31 10:29:30 -1000

    catching up, preparing for cron test

--- daily-data-aggregator/uploader.py
+++ daily-data-aggregator/uploader.py
@@ -4,11 +4,11 @@
 
 # Notes for continued implementation:
 # The message sent to the actor needs to be a JSON object that includes the following:
-# - ul_upload_url:  The base URL to access the science gateway that files will be stored in.
-# - ul_api_token:   The api token to run this uploader with.
-# - ul_ref_token:   The refresh token to refresh the api token, if this container
+# - gw_upload_url:  The base URL to access the science gateway that files will be stored in.
+# - gw_api_token:   The api token to run this uploader with.
+# - gw_ref_token:   The refresh token to refresh the api token, if this container
 #                   takes a very long time to execute.
-# - ul_dirs_to_upload:  A JSON object, serving as a list of the absolute paths
+# - gw_dirs_to_upload:  A JSON object, serving as a list of the absolute paths
 #                       of the directories we want to upload during this run.
 #                       Typically, this will be the same across runs unless
 #                       something is being debugged, tested, or added.
@@ -30,13 +30,9 @@ else:
     print("the message isn't in json format.")
     exit(1)
 
-upload_url      = msg_dict['ul_upload_url']
-api_token       = msg_dict['ul_api_token']
-ref_token       = msg_dict['ul_ref_token']
-dirs_to_upload = json.loads(msg_dict['ul_dirs_to_upload'])
-
-# TODO: Check if api_token will still be valid for the next 30 minutes,
-#       refresh using ref_token it if it won't.
+upload_url      = msg_dict['gw_upload_url']
+api_token       = msg_dict['gw_api_token']
+dirs_to_upload = json.loads(msg_dict['gw_dirs_to_upload'])
 
 
 #  If any of these files end up being very large, it is recommended to use stream
@@ -53,8 +49,8 @@ def upload_files_via_requests(upload_url, api_token, dirs_to_upload):
     mimetypes.add_type('text/csv', '.csv', strict=True)
 
     file_list = []
-    for directory in dirs_to_upload:
-        for file in os.listdir(path=directory):
+    for directory in dirs_to_upload: # need to ensure that directory has a slash at end - os.path.join (directory, filename)
+        for file in os.listdir(path=directory): # check for cases where directories are nested within the directories here
             # Append this file to the list of files.
             file_list.append(
                 ('file', (file, open(directory+file, 'rb'), mimetypes.guess_type(file)))
@@ -63,4 +59,6 @@ def upload_files_via_requests(upload_url, api_token, dirs_to_upload):
     print(res)
 
 # Finally, upload the files.
-upload_files_via_requests(upload_url, api_token, dirs_to_upload)
+res = upload_files_via_requests(upload_url, api_token, dirs_to_upload)
+
+# take action based on response (success, etc)
