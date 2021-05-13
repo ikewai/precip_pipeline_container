c527693db84c256f9ece11ddf3e1f96c5bbe8df1
Michael Dodge <mdodge@hawaii.edu>
2021-05-13 10:23:59 -1000

    changed json structure, updated uploader

--- gateway_scripts/uploader.py
+++ gateway_scripts/uploader.py
@@ -8,14 +8,20 @@
 # - gw_api_token:   The api token to run this uploader with.
 
 import json, os, datetime, mimetypes
+from typing import List
 from agavepy import Agave
 
 ag = Agave()
 ag.restore()
 ag.token.create()
+
+class fileNameAndPath:
+    fileName: str
+    filePath: str
+
 # Set up upload directories
 with open("file_upload_list.json") as file_list:
-    files_to_upload: list = json.loads(file_list.read())
+    files_to_upload: list[fileNameAndPath] = json.loads(file_list.read())['data']
 
 # map csv to text/csv instead of application/vnd.ms-excel
 mimetypes.add_type('text/csv', '.csv', strict=True)
@@ -29,11 +35,12 @@ ag.files.manageOnDefaultSystem(body={"action": "mkdir", "path": current_year}, s
 ag.files.manageOnDefaultSystem(body={"action": "mkdir", "path": current_month}, sourceFilePath=f"{upload_dir}/{current_year}")
 ag.files.manageOnDefaultSystem(body={"action": "mkdir", "path": current_day}, sourceFilePath=f"{upload_dir}/{current_year}/{current_month}")
 
+
+
 # Finally, upload the files.
 print(f"Files to upload: {files_to_upload}")
-for file_path in files_to_upload:
-    fileToUpload = open(file_path, 'rb')
-    #File Name may be autogen, file path will be based on run date, via the making directories
-    res = ag.files.importData(fileName="FILENAMEHERE", filePath="FILEPATHHERE", fileToUpload=fileToUpload)
+for fnap in files_to_upload:
+    fileToUpload = open(fnap.filePath, 'rb')
+    res = ag.files.importData(fileName=fnap.fileName, filePath="FILE PATH ON REMOTE HERE", fileToUpload=fileToUpload)
 
 # take action based on response (success, etc)
