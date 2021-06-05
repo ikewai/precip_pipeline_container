464b069432da884e08ea1e5a0d073fdbc90c0121
Michael Dodge <mdodge@hawaii.edu>
2021-06-05 09:33:04 -0400

    update downloader to make directory strings

--- gateway_scripts/downloader.py
+++ gateway_scripts/downloader.py
@@ -1,9 +1,5 @@
-# Needs to pull:
-# - Station list
-# - Previous day's cumulative data (if not first day of the month)
-
 from agavepy import actors, Agave
-import json, os, datetime
+import json, os, datetime, calendar
 
 ag = Agave()
 ag.restore()
@@ -18,8 +14,17 @@ isFirstDayOfMonth: bool = (current_day == 1)
 # Check if we're also at the start of the year, we'll want the last day of last year's data if so.
 isFirstMonthOfYear: bool = (current_month == 1)
 
+previous_day_data_dir: str = '' # YYYY/MM/DD-formatted, to match remote directory from uploader.
+
 if isFirstDayOfMonth and not isFirstMonthOfYear:
-    """set the appropriate directory variables"""
+    last_month = current_month - 1
+    last_day_of_last_month = calendar.monthrange(current_year, current_month)[1]
+    previous_day_data_dir = f"{current_year}/{last_month}/{last_day_of_last_month}"
 
 if isFirstDayOfMonth and isFirstMonthOfYear:
-    """set the appropriate directory variables"""
+    last_year = current_year - 1
+    last_month = 12 # December
+    last_day_of_last_month = 31 # Dec always ends in 31st
+    previous_day_data_dir = f"{last_year}/{last_month}/{last_day_of_last_month}"
+
+# Next, execute the actual downloading of the station list and the previous day's data.
